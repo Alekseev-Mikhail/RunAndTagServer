745279c253f0fa09f01230222455bcb8c004ef60
unit <waltz.no2.shostakovich@gmail.com>
2024-08-06 15:02:14 +0300

    Now ray step, max ray distance, and rotation velocity are defined by the server

--- GameMap.cs
+++ GameMap.cs
@@ -1,4 +1,5 @@
 using Core;
+using RunAndTagCore;
 
 namespace RunAndTagServer;
 
--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using Core;
+﻿using RunAndTagCore;
 using RunAndTagServer;
 
 const string tileSet = "###########" +
@@ -14,8 +14,26 @@ const string tileSet = "###########" +
                        "###########";
 const int mapWidth = 11;
 
-var seeker = new Player(5f, 2.5f, 165f, 0.03f);
-var hider = new Player(4f, 7f, 90f, 0.01f);
+var seeker = new Player(
+    5f,
+    2.5f,
+    165f,
+    0.01f,
+    5f,
+    75f,
+    3f,
+    0.03f
+);
+var hider = new Player(
+    4f,
+    7f,
+    90f,
+    0.01f,
+    1f,
+    25f,
+    3f,
+    0.01f
+);
 var map = new GameMap(tileSet, mapWidth, '#', seeker, hider);
 
 var game = new Game(map);
